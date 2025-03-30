e649557019aca4e67171b96e247eb1fb93e09ceb
p-nayak11 <prabhanjanmnayak@gmail.com>
2025-03-30 19:38:10 -0400

    switching to poetry

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ create --name ml_sec_proj python=3.11
 conda activate ml_sec_proj
 
 pip install -U "jax[cuda12]"
-pip install jaxmarl
 pip install -e .[algs]
 
 export PYTHONPATH=./JaxMARL:$PYTHONPATH
