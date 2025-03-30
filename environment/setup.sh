2b6b37b97225bf4e4276ad28c0280932f7a70710
p-nayak11 <prabhanjanmnayak@gmail.com>
2025-03-30 16:14:14 -0400

    debugging the env setup

--- environment/setup.sh
+++ environment/setup.sh
@@ -4,7 +4,10 @@ cd "$(dirname "$0")"
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate ml_sec_project
 
+pip install --upgrade pip
+pip install --upgrade "jax[cuda12]"
 pip install -e .[algs]
+
 export PYTHONPATH=./JaxMARL:$PYTHONPATH
 
 echo "Environment setup complete."
